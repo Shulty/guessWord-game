70131dbc5226bd47822bf00db723a5e5c1a2d730
Maxim <hype746@gmail.com>
2026-02-05 21:10:11 +0300

    feat: random word and change logic to class

--- src/index.js
+++ src/index.js
@@ -1,47 +1,22 @@
+import { Game } from "./components/game.js";
 const menuEl = document.getElementById('menu');
 const playBtn = document.querySelector('.menu__start-btn');
 
-const gameEl = document.getElementById('game');
 
-const inputs = document.querySelectorAll('.code-input');
-
-inputs.forEach((input, index) => {
-  input.addEventListener('input', () => {
-    if (input.value.length === 1 && index < inputs.length - 1) {
-      inputs[index + 1].focus();
-    }
-  });
-  input.addEventListener('keydown',(e)=>{
-    if(e.key === 'Backspace' && input.value === '' && index>0){
-        inputs[index - 1].focus();
-    }
-  })
-});
-inputs[inputs.length-1].addEventListener('keydown',(e)=>{
-    if(e.key === 'Enter'){
-        makeWordRight(inputs);
-    }
-})
+playBtn.addEventListener('click',()=>startGame());
 
 
-
-playBtn.addEventListener('click',()=>startGame("snake"));
-
-
-async function startGame(word){
+async function startGame(){
     menuEl.style.opacity = 0;
     setTimeout(()=>{
         menuEl.style.display = 'none';
-        gameEl.style.display = 'flex';
-        gameEl.style.transform = 'translateY(0px)';
+        const game = new Game(randomWord());
     },800);
     
 
 }
-function makeWordRight(inputs){
-    inputs.forEach((el, index) => {
-         setTimeout(() => {
-             el.classList.toggle("code-input-right"); 
-            }, index * 500); 
-        });
+function randomWord(){
+    let words = ['parrot','cooker','captain','lemon','cat','bus','slap','swap','pool','kids','letter','container','mouse','street','this','object','code','script','apple','pineapple','loser','winner','happy','smooth','tea','pot','teapot','honey','worker','build','brick'];
+    let id = Math.floor(Math.random()*(words.length+1));
+    return words[id];
 }
